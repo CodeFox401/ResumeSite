336077f17acc3deb94019a5394a5963465762cc9
John Chiki <Tylervette2016@gmail.com>
2024-07-14 19:04:38 -0400

    updated darkmode stuff

--- darkmode.js
+++ darkmode.js
@@ -8,17 +8,20 @@ if (currentTheme == "dark") {
   document.body.classList.toggle("light-theme");
 }
 
-btn.addEventListener("click", function () {
-  if (prefersDarkScheme.matches) {
-    document.body.classList.toggle("light-theme");
-    var theme = document.body.classList.contains("light-theme")
-      ? "light"
-      : "dark";
-  } else {
-    document.body.classList.toggle("dark-theme");
-    var theme = document.body.classList.contains("dark-theme")
-      ? "dark"
-      : "light";
-  }
-  localStorage.setItem("theme", theme);
-});
+btn.addEventListener("click", toggleDarkMode())
+
+function toggleDarkMode()
+{
+    if (prefersDarkScheme.matches) {
+        document.body.classList.toggle("light-theme");
+        var theme = document.body.classList.contains("light-theme")
+          ? "light"
+          : "dark";
+      } else {
+        document.body.classList.toggle("dark-theme");
+        var theme = document.body.classList.contains("dark-theme")
+          ? "dark"
+          : "light";
+      }
+      localStorage.setItem("theme", theme);
+}
