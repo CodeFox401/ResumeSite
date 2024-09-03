698f574012c71cc63b67fbeb2c2e8f161163df72
John Chiki <Tylervette2016@gmail.com>
2024-09-03 18:26:09 -0400

    Fixed the button icon not changing on click

--- darkmode.js
+++ darkmode.js
@@ -1,29 +1,17 @@
-const btn = document.querySelector(".btn-toggle-darkmode");
-const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)");
+document.addEventListener('DOMContentLoaded', () => {
+    const btn = document.getElementById('btn-toggle-darkmode');
+    const themeIcon = document.getElementById('theme-toggle-img');
+    const lightModeIconSrc = 'images/light-mode.png';
+    const darkModeIconSrc = 'images/dark-mode.png';
 
-const currentTheme = localStorage.getItem("theme");
-if (currentTheme == "dark") {
-  document.body.classList.toggle("dark-theme");
-} else if (currentTheme == "light") {
-  document.body.classList.toggle("light-theme");
-}
+    function toggleDarkMode() {
+        if (themeIcon.src.endsWith(lightModeIconSrc)) {
+            themeIcon.src = darkModeIconSrc;
+        } else {
+            themeIcon.src = lightModeIconSrc;
+        }
+    }
 
-btn.addEventListener("click", toggleDarkMode())
-
-function toggleDarkMode()
-{
-    if (prefersDarkScheme.matches) {
-        document.body.classList.toggle("light-theme");
-        var theme = document.body.classList.contains("light-theme")
-          ? "light"
-          : "dark";
-          document.getElementById('darkmodeIcon').src = 'images/light-mode.png';
-      } else {
-        document.body.classList.toggle("dark-theme");
-        var theme = document.body.classList.contains("dark-theme")
-          ? "dark"
-          : "light";
-          document.getElementById('darkmodeOff') = 'images/dark-mode.png';
-      }
-      localStorage.setItem("theme", theme);
-}
+    // Add the event listener with the function reference
+    btn.addEventListener('click', toggleDarkMode);
+});
